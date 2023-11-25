09e056e6b6467fde9fdcfde9833b41c32daca315
Andre Dias <andred_nunes@hotmail.>
2023-11-25 11:57:34 -0300

    learning controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,15 +5,19 @@ const mongoose = require('mongoose');
 // get all workouts
 const getWorkouts = async (req, res) =>{
     const workouts = await Workout.find({}).sort({createdAt: -1});
-    res.status(200).json(workouts)
+    res.status(200).json(workouts);
 }
 
 // get a single workout
 
 const getWorkout = async (req,res)=>{
     const { id } = req.params
-    
-    const workout = await Workout.findById(id)
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({error: 'No such workout'});
+    }
+
+
+    const workout = await Workout.findById(id);
 
     if (!workout){
         return res.status(404).json({error: 'no such workout'});
