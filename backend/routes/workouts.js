f2b32ec1d4c8ac93ebd5b362d60acf757c8be040
Andre Dias <andred_nunes@hotmail.>
2023-11-24 23:38:10 -0300

    learning MERN stack

---
+++ backend/routes/workouts.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Workout = require('../models/workoutModel')
+const {createWorkout,getWorkout, getWorkouts} = require('../controllers/workoutController')
+
+const router = express.Router();
+
+// GET all workouts
+router.get('/',getWorkouts);
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout);
+
+// DELETE a new workout
+router.delete('/:id',(req, res)=>{
+    res.json({mssg:'DELETE a new workout'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res)=>{
+    res.json({mssg:'UPDATE a  workout'})
+})
+
+module.exports = router;
