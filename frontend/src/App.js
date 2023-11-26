4bf5655833c957d2f66064c591fdc3c1ead26888
Andre Dias <andred_nunes@hotmail.>
2023-11-26 13:18:41 -0300

    atualizando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 // pages and components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
